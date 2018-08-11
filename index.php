d650c2cd546702e7613a20228117d16aca6f58f3
erratic <erratic@shells.yourstruly.sx>
2018-08-11 00:08:20 +0000

    added settings, index

--- index.php
+++ index.php
@@ -1,5 +1,34 @@
 <?php
 
+define('PSCR_LIB_ROOT', './');
+
+define('PSCR_PROJECT_ROOT', './');
+
+
 require_once('vendor/autoload.php');
 
-pscr\lib\logging\logger::_()->info($this, "wtf");
+\pscr\lib\logging\logger::_()->info("-----------------------------------------new request------------------------------------------", isset($_SERVER['REQUEST_URI']))
+    ? $_SERVER['REQUEST_URI']
+    : $_SERVER['argv'];
+
+if (substr(php_sapi_name(), 0, 3) == 'cgi') {
+    http_request_handler();
+}
+else if (php_sapi_name() == 'fpm-fcgi') {
+    http_request_handler();
+}
+else if (php_sapi_name() == 'cli-server') {
+    http_request_handler();
+}
+else if (php_sapi_name() == "cli") {
+    (new pscr\lib\management\arguments())->execute($_SERVER['argv'][0],
+        array_slice($_SERVER['argv'], 1));
+}
+else {
+    \pscr\lib\logging\logger::_()->info("MAIN", "BUG: php_sapi_name unhandled or null");
+}
+
+\pscr\lib\logging\logger::_()->info("-----------------------------------------finished request------------------------------------------", isset($_SERVER['REQUEST_URI']))
+    ? $_SERVER['REQUEST_URI']
+    : $_SERVER['argv'];
+
